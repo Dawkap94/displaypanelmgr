27f988d5dde8f5628ffaa92500d90a66a5d9ab38
DawKap94 <dawidkapusta67@gmail.com>
2025-01-08 20:47:22 +0100

    forms and requirements

--- users/forms.py
+++ users/forms.py
@@ -1,17 +1,19 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
 
 from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
-
     class Meta:
         model = CustomUser
         fields = ("email",)
+        widgets = {
+            'email': forms.EmailInput(attrs={'class': 'my-email-class'})
+        }
 
 
 class CustomUserChangeForm(UserChangeForm):
-
     class Meta:
         model = CustomUser
         fields = ("email",)
